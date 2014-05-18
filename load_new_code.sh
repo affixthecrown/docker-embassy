29b8797183c264f8755dbc581e3bb61c2f77c9f0
Brent Roberts <affixthecrown@gmail.com>
2014-05-18 21:15:38 +0000

    adding scripting to rebuild container with new code

---
+++ load_new_code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Getting current container id..." &&\
+ID=$(sudo docker ps | grep embassy | awk '{printf $1}') && \
+echo "Kill id $ID" && \
+sudo docker kill $ID && \
+echo "Beginning build of new container..." && \
+sudo docker build -t embassy/app . && \
+echo "Running new container..." && \
+sudo docker run -p 5080:80 -d embassy/app
+
