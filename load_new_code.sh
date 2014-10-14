f7804b5c33e056ce1f9c483eea911d03029ae0fa
Brent Roberts <affixthecrown@gmail.com>
2014-10-14 14:51:26 +0000

    tweaks to container build scripts

--- load_new_code.sh
+++ load_new_code.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "Getting current container id..." &&\
-ID=$(sudo docker ps | grep embassy | awk '{printf $1}') && \
-sudo docker kill $ID && \
-echo "Beginning build of new container..." && \
-sudo docker build -t embassy/app . && \
-echo "Running new container..." && \
-sudo docker run -p 5080:80 -d embassy/app
+
+echo "Setting up the web share..."
+./setup_share.sh
+echo "Looking for current container id..."
+ID=$(sudo docker ps | grep embassy | awk '{printf $1}')
+[[ -n $ID ]] && sudo docker kill $ID 
+echo "Beginning build of new container..."
+sudo docker build -t embassy/app .
+echo "Running new container..."
+sudo docker run -p 80:80 -d ourbucketlist/app
 
--- setup_share.sh
+++ setup_share.sh
@@ -9,7 +9,6 @@ cp $SCRIPTS_DIR/*.js share/scripts/
 [[ -e js/common.js ]] && rm share/scripts/common.js
 
 for i in $(ls $SCRIPTS_DIR/common); do
-  echo $i
   cat $SCRIPTS_DIR/common/$i >> share/scripts/common.js
 done
 
